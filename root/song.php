122a276eb20d92ef726537082f2ba5d981797a1b
Abubakar Hassan <abhasy49@gmail.com>
2016-10-04 21:11:07 +0100

    Update song.php

--- root/song.php
+++ root/song.php
@@ -53,16 +53,7 @@ include("connect.php")
 	</main>
 	<footer>Designed Hassan</footer>
 <?php
-/*
-Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password)
-*/
-$link = mysqli_connect("localhost", "root", "pass", "album");
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+
  
 // Escape user inputs for security
 $id = mysqli_real_escape_string($link, $_POST['id']);

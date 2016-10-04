f87c1038274d08a63fe0fe7bcb5fe10dc76ad81d
Abubakar Hassan <abhasy49@gmail.com>
2016-10-04 21:08:25 +0100

    Add files via upload

--- root/index.php
+++ root/index.php
@@ -1,5 +1,5 @@
 <?php 
-include("connect.php")
+//include("connect.php")
  ?>
 
 
@@ -24,6 +24,7 @@ include("connect.php")
 		 <li><a href="index.php">Artiste</a></li>
 		 <li><a href="genre.php">Genre</a></li>
 		  <li><a href="song.php">Songs</a></li>
+                  <li><a href="view.php">View</a></li>
 		 <div style="clear:both;"></div>
 		</ul>
        </nav>
@@ -41,32 +42,35 @@ include("connect.php")
                 </tr>
                 <tr>
 		 <td><label for="group">Music Group</label></td>
-		 <td> <input type="text" name="group" id="group"  placeholder="Music Group" required></td>
+		 <td> <input type="text" name="music_group" id="music_group"  placeholder="Music Group" required></td>
                 </tr>
                 <tr>
                  <td></td>
                  <td><input type="submit" value="Insert track"></td>
                 </tr>
 		</table>
+<?php
+
+// Escape user inputs for security
+$id = mysqli_real_escape_string($link, $_POST['']);
+$name = mysqli_real_escape_string($link, $_POST['name']);
+$dob = mysqli_real_escape_string($link, $_POST['dob']);
+$group = mysqli_real_escape_string($link, $_POST['music_group']);
+ 
+// attempt insert query execution
+$sql = "INSERT INTO artiste (id, name, DOB,music_group) VALUES ('$id', '$name', '$dob','$group')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// close connection
+mysqli_close($link);
+?>
           </form>
 	</main>
 	<footer>Designed Hassan</footer>
-<?php
-$name = $_POST["name"];
-$dob = $_POST["dob"];
-$group = $_POST["group"];
-
-
-$sql = "INSERT INTO artiste (name,dob,music_group)
-VALUES ($name, $dob, $group)";
-
-if (mysqli_query($conn, $sql)) {
-    echo "New artiste created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-?>
 
 </div>
 </body>

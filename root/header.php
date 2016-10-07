3161829acb8c44b6b474cdae99d2b7cfb179a112
Abubakar Hassan <abhasy49@gmail.com>
2016-10-07 09:03:14 +0100

    Add files via upload

---
+++ root/header.php
@@ -0,0 +1,17 @@
+<header>
+    <div id="head1">
+        <img src="img/albm.jpeg" alt="logo">
+    </div>
+    <div id="head">
+        <h3>Q-album</h3>
+    </div>
+</header>
+<nav>
+    <ul>
+        <li><a href="index.php">Artiste</a></li>
+        <li><a href="genre.php">Genre</a></li>
+        <li><a href="song.php">Songs</a></li>
+        <li><a href="view.php">View</a></li>
+        <div style="clear:both;"></div>
+    </ul>
+</nav>
